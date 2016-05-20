7817adde2c270017a63886b00abecd20eabb9953
SilvanRitz <silvan.ritz@stud.hslu.ch>
2016-05-20 11:33:13 +0200

    Akkuüberwachung eingeschaltet

--- MC_PREN_39/Sources/config.h
+++ MC_PREN_39/Sources/config.h
@@ -63,7 +63,7 @@ volatile bool IR_flanke;
  * Aktionen
  */
 #define FLEX_LENK_ENABLE			(0)			//Schaltet die Flexlenkung aus
-#define AKKU_ABSCHALTEN				(0)			//Akku überwachung
+#define AKKU_ABSCHALTEN				(1)			//Akku überwachung
 
 
 //TEST's	Im normalfall 0 !
